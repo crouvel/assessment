49a27b2bca14e64aa8c3a7306bf0f0de18c39feb
clarencerouvel <clarence.rouvel@acelys.fr>
2022-07-30 20:40:04 +0200

    end post details

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -15,13 +15,16 @@ const PostCard = (props) => {
         <div className="card card-2">
           <div className="card__icon"><i className="fas fa-bolt"></i></div>
           <p className="card__exit"><i className="fas fa-times"></i></p>
+           <div className="sub">
           <h2 className="card__title text-center"><img
             src={data.author.avatar}
             className="photo"
           /><b>{data.title}</b></h2>
           <h4 className="text-center pb-1">Written by <b>{data.author.name}</b></h4>
           <h4 className="pdate text-center"><b>Published by the </b>{data.publishDate.slice(0, 10)}</h4>
+          </div>
           <div className="card__apply">
+            <u>Click to see details</u>
           </div>
         </div>
       </Link>
--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -17,10 +17,10 @@ function PostDetail() {
         axios(`/api/posts/`)
             .then((response) => {
                 console.log(response.data.posts.filter(obj => {
-                    return obj.id == id
+                    return obj.id === id
                 }));
                 setRow(response.data.posts.filter(obj => {
-                    return obj.id == id
+                    return obj.id === id
                 }))
             })
             .catch((error) => {
@@ -37,10 +37,24 @@ function PostDetail() {
             return (
                 <article className="text-center">
                     <h1 className="text-center mt-4 font-italic"><b>Post Details:</b></h1>
-                    <h2 className="text-center mt-3 font-italic"><b>{row[0].title}</b></h2>
-                    <h4 className="text-center mt-3 mw-20">{row[0].summary}</h4>
-                    <h4 className="text-center mt-5"><b>Categories:</b></h4>
-                    {row[0].categories.map((category) => <h5 className="text-center"><i>{category.name}</i></h5>)}
+                    <div className="details">
+                        <div className="sub1">
+                            <h2 className="text-center mt-4 font-italic"><b>{row[0].title}</b></h2>
+                            <h4 className="text-center mt-4 mw-20">{row[0].summary}</h4>
+                        </div>
+                        <div>
+                            <h4 className="mt-3"></h4>
+                            <h4 className="text-center"><b></b>By: <img
+                                src={row[0].author.avatar}
+                                className="photo"
+                            /><b>{row[0].author.name}</b></h4>
+                        </div>
+                        <div className="sub1">
+                            <h4 className="text-center mt-3"><b>Categories:</b></h4>
+                            {row[0].categories.map((category) => <h5 className="text-center"><i>{category.name}</i></h5>)}
+                        </div>
+                        <h4 className="pdate text-center mt-2"><b>Published: </b><i>{row[0].publishDate.slice(0, 10)}</i></h4>
+                    </div>
                 </article>
             );
         } else {
